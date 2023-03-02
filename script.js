de4f57041b802179f152e4ba582d237192800a6d
rtolefree1 <r_ryde2000@yahoo.com>
2023-03-01 22:50:58 -0500

    updated index and script with military time and now udate of past, present and future is working

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-// let btnEL = $('.saveBtn')
-
-
-
-// btnEL.on('click', function () {
-//   let value = $(this).sibling('.description').val()
-// })
-
-
-
-
-
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 let timeNowEl = $('#currentDay');
 let timeVar = dayjs().format('hh:mm:ss');
 let dayVar = dayjs().format('dddd') + ' ' + dayjs().format('MMMM') + ' ' + dayjs().format('DD');
-
-// console.log("current time:", dayjs().format());
-// console.log("current time:", dayjs().hour()+":"+dayjs().minute());
 timeNowEl.text(dayVar);
 
 
@@ -27,22 +9,27 @@ let hour9El = $('#hour-9');
 let hour10El = $('#hour-10');
 let hour11El = $('#hour-11');
 let hour12El = $('#hour-12');
-let hour1El = $('#hour-1');
-let hour2El = $('#hour-2');
-let hour3El = $('#hour-3');
-let hour4El = $('#hour-4');
-let hour5El = $('#hour-5');
+let hour1El = $('#hour-13');
+let hour2El = $('#hour-14');
+let hour3El = $('#hour-15');
+let hour4El = $('#hour-16');
+let hour5El = $('#hour-17');
 let saveEl =  $('save');
 
+// creating local storage object will be empty on the first pass
+// after text is added then local storage will have event information
 hour9El.children().eq(1).val(JSON.parse(localStorage.getItem("calendar")));
 console.log(hour9El.children().eq(1).val(JSON.parse(localStorage.getItem("calendar"))));
 // creating array for the calender to hold an array of calendar objects
 //--9th hour --
-let calenderArr = [];
+let calenderArr = []; //array for the calendar objects
+
 function saveHour9(){
+  // getting text for the event input
   let hour9Text = hour9El.children().eq(1).val();
   console.log(hour9Text);
   
+  //pushing data to array the setting to local storage
   calenderArr.push(hour9Text);
   localStorage.setItem("calendar", JSON.stringify(calenderArr));
 
@@ -50,13 +37,21 @@ timeForStatus();
 }
 hour9El.children().eq(2).on('click', saveHour9);
 
+
+
 //--10th hour --
+// creating array for the calender to hold an array of calendar objects
 let calenderArr2 = [];
+
+// creating local storage object will be empty on the first pass
+// after text is added then local storage will have event information
 hour10El.children().eq(1).val(JSON.parse(localStorage.getItem("calendar2")));
 function saveHour10(){
+  // getting text for the event input
   let hour10Text = hour10El.children().eq(1).val();
   console.log("Doing work at the 10am hour");
  
+   //pushing data to array the setting to local storage
  calenderArr2.push(hour10Text);
  localStorage.setItem("calendar2", JSON.stringify(calenderArr2));
 }
@@ -64,16 +59,20 @@ hour10El.children().eq(2).on('click', saveHour10);
 
 
 //--11th hour --
-let calenderArr3 = [];
+let calenderArr3 = []; //array for the calendar objects
+// creating local storage object will be empty on the first pass
+// after text is added then local storage will have event information
 hour11El.children().eq(1).val(JSON.parse(localStorage.getItem("calendar3")));
 function saveHour11(){
+
+  // getting text for the event input
   let hour11Text = hour11El.children().eq(1).val();
   console.log("Doing work at the 11am hour");
- 
+
+ //pushing data to array the setting to local storage
   calenderArr3.push(hour11Text);
   localStorage.setItem("calendar3", JSON.stringify(calenderArr3));
 
-  // timeForStatus("1", hour11El);
 }
 hour11El.children().eq(2).on('click', saveHour11);
 
@@ -161,79 +160,20 @@ function saveHour5(){
 hour5El.children().eq(2).on('click', saveHour5);
 
 
-
-
-
-
-
-// $(function () {
-//   // TODO: Add a listener for click events on the save button. This code should
-//   // use the id in the containing time-block as a key to save the user input in
-//   // local storage. HINT: What does `this` reference in the click listener
-//   // function? How can DOM traversal be used to get the "hour-x" id of the
-//   // time-bl for ock containing the button that was clicked? How might the id be
-//   // useful when saving the description in local storage?
-//   //
-  
-
-//   // TODO: Add code to apply the past, present, or future class to each time
-//   // block by comparing the id to the current hour. HINTS: How can the id
-//   // attribute of each time-block be used to conditionally add or remove the
-//   // past, present, and future classes? How can Day.js be used to get the
-//   // current hour in 24-hour time?
-//   //
-//   // TODO: Add code to get any user input that was saved in localStorage and set
-//   // the values of the corresponding textarea elements. HINT: How can the id
-//   // attribute of each time-block be used to do this?
-//   //
-//   // TODO: Add code to display the current date in the header of the page.
-// });
-
 var objTimeArr = [hour9El, hour10El, hour11El, hour12El, hour1El, hour2El, hour3El, hour4El, hour5El]
-var timeArr = ["9","10","11","12","1","2","3","4","5"];
-//  var timeArrMorning = ["9","10","11"];
-//  var timeArrEvening = ["12","1","2","3","4","5"];
+var timeArr = ["9","10","11","12","13","14","15","16","17"];
+
 
 let date = new Date();
 let currentTime = date.toLocaleTimeString();
 let morningEveningValue = date.toLocaleTimeString().split(" ")[1];
-if(morningEveningValue === "AM")
-    {
-      for(let i = 0; i < 3; i++)
- {
-   timeForStatus(timeArr[i], objTimeArr[i]);
 
- }
-    }
-    else if(morningEveningValue === "PM")
-    {
-      for(let i = 3; i < objTimeArr.length; i++)
+
+for(let i = 0; i < objTimeArr.length; i++)
       {
-        timeForStatus(timeArr[i], objTimeArr[i]);
+       timeForStatus(timeArr[i], objTimeArr[i]);
       }
-    }
 
-// for(let i = 0; i < objTimeArr.length; i++)
-// {
-//   timeForStatus(timeArr[i], objTimeArr[i]);
-
-// }
-
-function setStatusMorning(){
-  for(let i = 0; i < 3; i++)
- {
-   timeForStatus(timeArr[i], objTimeArr[i]);
-
- }
-}
-
-function setStatusEvening(){
-  for(let i = 3; i < objTimeArr.length; i++)
- {
-   timeForStatus(timeArr[i], objTimeArr[i]);
-
- }
-}
 
 
 //Creating timer
@@ -241,68 +181,31 @@ function timeForStatus(timeValue, obj){
     //debugger;
     let date = new Date();
     let currentTime = date.toLocaleTimeString();
-    // let morningEveningValue = date.toLocaleTimeString().split(" ")[1];
+    let currentTime2 = dayjs().format('HH');
+    console.log("timeValue",timeValue);
+    console.log("currentTime2", currentTime2);
+    
+    // if time is after 5pm lets set value to 6pm
+    // that way all is in past
+    if(parseInt(currentTime2)>17){
+      currentTime2 = 18;
+    }
+    console.log("current time 2", currentTime);
 
-    if(morningEveningValue === "AM")
-    {
-      if (parseInt(timeValue) < parseInt(currentTime)) {
+    // checking values to set past, preset or future 
+      if (parseInt(timeValue) < parseInt(currentTime2)) {
         $(obj).addClass("past");
         $(obj).removeClass("future");
         $(obj).removeClass("present");
-      } else if (parseInt(timeValue) > parseInt(currentTime)) {
+      } else if (parseInt(timeValue) > parseInt(currentTime2)) {
+        $(obj).removeClass("past");
+        $(obj).addClass("future");
+        $(obj).removeClass("present");
+      } else {
+        $(obj).removeClass("future");
         $(obj).removeClass("past");
         $(obj).addClass("present");
-        $(obj).removeClass("future");
-      } else {
-        $(obj).removeClass("present");
-        $(obj).removeClass("past");
-        $(obj).addClass("future");
       }
-    }else if(morningEveningValue === "PM")
-    {
-      if (parseInt(timeValue) < parseInt(currentTime)) {
-        $(obj).addClass("past");
-        $(obj).removeClass("future");
-        $(obj).removeClass("present");
-      } else if (parseInt(timeValue) > parseInt(currentTime)) {
-        $(obj).removeClass("past");
-        $(obj).addClass("future");
-        $(obj).removeClass("present");
-      } else {
-        $(obj).removeClass("present");
-        $(obj).removeClass("past");
-        $(obj).addClass("future");
-      }
-    }
-
-}
-
-// printCalendarEvent(){
-
-// }
-// var clock = setInterval(myTimer, 1000);
-
-
-
-// function myTimer() {
-//   let date = new Date();
-//   let time = date.toLocaleTimeString();
-//   document.getElementById("clock").innerHTML = time;
-//   $(".time-block").each(function () {
-//     var clockNow = parseInt($(this).attr("id"));
-
-//     if (clockNow < time) {
-//       $(this).addClass("past");
-//       $(this).removeClass("future");
-//       $(this).removeClass("present");
-//     } else if (clockNow > time) {
-//       $(this).removeClass("past");
-//       $(this).addClass("present");
-//       $(this).removeClass("future");
-//     } else {
-//       $(this).removeClass("present");
-//       $(this).removeClass("past");
-//       $(this).addClass("future");
-//     }
-//   });
-// }
+    
+  }
+    
